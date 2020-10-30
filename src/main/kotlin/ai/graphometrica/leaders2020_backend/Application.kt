83963dd8f0a4a8ee55ad99bf296dfd245bd38941
ruapgs9 <Sergey.PINIGIN@raiffeisen.ru>
2020-10-30 22:19:31 +0300

    Add project

---
+++ src/main/kotlin/ai/graphometrica/leaders2020_backend/Application.kt
@@ -0,0 +1,11 @@
+package ai.graphometrica.leaders2020_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Leaders2020BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<Leaders2020BackendApplication>(*args)
+}
